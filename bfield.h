cb0757753b7290fb27f42855db871e7ad328e8a3
hiddevuijk <hdvuijk@gmail.com>
2019-01-23 11:10:30 +0100

    input to f is XYZ object, removed t dep.

--- bfield.h
+++ bfield.h
@@ -2,11 +2,11 @@
 #define GUARD_BFIELD_H
 
 #include <vector>
-
+#include "xyz.h"
 
 class Bfield {
 public:
-	virtual double f(const std::vector<double>& r,double t) = 0; 
+	virtual double f(const XYZ& r) = 0; 
 
 protected:
 	double B;
@@ -17,7 +17,7 @@ protected:
 };
 
 class BNone: public Bfield {
-	double f(const std::vector<double>& r,double t)
+	double f(const XYZ& r)
 			{return 0;}
 };
 
@@ -26,8 +26,8 @@ public:
 	Bsigmoid(double b0, double b, double l) {
 			B = b; B0 = b0; L=l;}
 
-	double f(const std::vector<double>& r, double t) {
-			return B0*tanh(B*(r[1]-L/2.));}
+	double f(const XYZ& r) {
+			return B0*tanh(B*(r.y-L/2.));}
 
 };
 
@@ -37,27 +37,15 @@ public:
 	Bsaw(double b0, double b, double l) {
 			B = b; B0 = b0; L=l;}
 
-	double f(const std::vector<double>& r, double t) {
-			if(r[1]<L/4.) {
-				return B0+B*0.25*L-B*(r[1]-0.25*L);	
-			} else if(r[1]>3*L/4.) {
-				return B0+B*0.75*L-B*(r[1]-0.75*L);
+	double f(const XYZ& r) {
+			if(r.y<L/4.) {
+				return B0+B*0.25*L-B*(r.y-0.25*L);	
+			} else if(r.y>3*L/4.) {
+				return B0+B*0.75*L-B*(r.y-0.75*L);
 			} else {
-				return B0+B*r[1];
+				return B0+B*r.y;
 			}
 	}
-
-};
-
-
-
-class BsinYt: public Bfield {
-public:
-	BsinYt(double BB, double ww, double wtt) {
-		B = BB; w=ww; wt=wtt;}
-
-	double f(const std::vector<double>& r,double t) {
-			return B*std::sin(w*(r[1]-wt*t)); }
 
 };
 
@@ -69,8 +57,8 @@ public:
 	BsinY(double BB, double ww) {
 		B = BB; w=ww; }
 
-	double f(const std::vector<double>& r,double t) {
-			return B*std::sin(w*r[1]); }
+	double f(const XYZ& r) {
+			return B*std::sin(w*r.y); }
 
 };
 
@@ -79,8 +67,8 @@ public:
 	BlinearY(double b0, double b) {
 			B = b; B0 = b0;}
 
-	double f(const std::vector<double>& r, double t) {
-			return B0+B*r[1];}
+	double f(const XYZ& r) {
+			return B0+B*r.y;}
 
 };
 
@@ -89,15 +77,13 @@ public:
 	BlinearR(double b0,double b,double l) {
 		B = b; B0 = b0; L = l;}
 
-	double f(const std::vector<double>& r,double t) {
-		double d = sqrt( (r[0]-L/2)*(r[0]-L/2) + 
-						 (r[1]-L/2)*(r[1]-L/2));
+	double f(const XYZ& r) {
+		double d = sqrt( (r.x-L/2)*(r.x-L/2) + 
+						 (r.y-L/2)*(r.y-L/2));
 		return B0+B*d;
 	}
 
 };
-
-
 
 
 
