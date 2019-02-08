de3fd286782a1c1a3cb502a42caaf7830c74268c
hiddevuijk <hdvuijk@gmail.com>
2019-02-08 13:06:33 +0100

    no changes

--- plot.py
+++ plot.py
@@ -2,6 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sys import exit
 
+plt.subplot(1,2,1)
+presX = np.loadtxt("pressureX.dat")
+plt.imshow(presX,interpolation="none")
+plt.colorbar()
+
+plt.subplot(1,2,2)
+presY = np.loadtxt("pressureY.dat")
+plt.imshow(presY,interpolation="none")
+plt.colorbar()
+
+plt.show()
+
+
 rho = np.loadtxt("rho.dat")
 bins = np.loadtxt("rho_bins.dat")
 
@@ -15,18 +28,24 @@ pz = np.loadtxt("pz.dat")
 
 plt.subplot(2,3,1)
 plt.imshow(rho,interpolation='none')
+plt.title("density")
+plt.colorbar()
 
 plt.subplot(2,3,2)
 plt.imshow(fx,interpolation='none')
+plt.title("flux x")
 
 plt.subplot(2,3,3)
 plt.imshow(fy,interpolation='none')
+plt.title("flux Y")
 
 plt.subplot(2,3,5)
 plt.imshow(px,interpolation='none')
+plt.title("p x")
 
 plt.subplot(2,3,6)
 plt.imshow(py,interpolation='none')
+plt.title("p y")
 
 plt.show()
 
--- plot_config.py
+++ plot_config.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sys import exit
 
-r = np.loadtxt("final_config.dat")
+r = np.loadtxt("initial_config.dat")
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
@@ -11,9 +11,9 @@ ax = fig.add_subplot(111,projection='3d')
 for i in range(r.shape[0]):
 	ax.scatter(r[i,0],r[i,1],r[i,2])
 
-ax.set_xlim([0,10])
-ax.set_ylim([0,10])
-ax.set_zlim([0,10])
+#ax.set_xlim([0,10])
+#ax.set_ylim([0,10])
+#ax.set_zlim([0,10])
 
 plt.show()
 
